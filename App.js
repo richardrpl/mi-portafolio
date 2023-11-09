2e1b01798bba8f61dc6a5f54df421da877c8e568
Richard <[200~115833194+richardrpl@users.noreply.github.com>
2023-11-08 23:08:20 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+document.getElementById("correo").addEventListener("click", function(){
+    var correoElectronico = 'richardperezlanguasco@gmail.com';
+
+    window.location.href = 'mailto:' + correoElectronico;
+});
+
+
+document.getElementById("btn-habilidades").addEventListener("click", function() {
+var boton = document.getElementById("imagenes");
+
+if(boton.style.display==="none"){
+    boton.style.display = "block"
+} else {
+    boton.style.display = "none"
+}
+});
+
+document.getElementById("btn-certificados").addEventListener("click", function() {
+var btn = document.getElementById("cont-certificados");
+if(btn.style.display === "none") {
+    btn.style.display = "block"
+} else {
+    btn.style.display = "none"
+}
+});
+
+document.getElementById("descargarcv").addEventListener("click", function() {
+    window.location.href = "https://drive.google.com/file/d/1_CbF21txri1xRthZv5eyD7aRPKeVy883/view?usp=sharing";
+});
