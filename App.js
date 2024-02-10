c8fbb8dea31d159754a62ea1711dedc00fa9b105
Richard <[200~115833194+richardrpl@users.noreply.github.com>
2024-02-10 15:12:36 -0400

    terminando el responsive

--- App.js
+++ App.js
@@ -24,6 +24,15 @@ if(btn.style.display === "none") {
 }
 });
 
+document.getElementById("btn-proyectos").addEventListener("click", function() {
+    var btn = document.getElementById("contenedor-proyectos-img");
+    if(btn.style.display ==="none") {
+        btn.style.display = "block"
+    } else {
+        btn.style.display = "none"
+    }
+})
+
 document.getElementById("descargarcv").addEventListener("click", function() {
     window.location.href = "https://drive.google.com/file/d/1_CbF21txri1xRthZv5eyD7aRPKeVy883/view?usp=sharing";
 });
